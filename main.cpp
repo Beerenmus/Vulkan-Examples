d9673bb9e46053dd15bfe2c3afe87a05d2074081
Beerenmus <m.buhl1@web.de>
2023-08-18 19:03:18 +0200

    Window created

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
+#include <iostream>
 #include <GLFW/glfw3.h>
 
 int main() {
-    return 0;
+
+    if(glfwInit() == GLFW_FALSE) {
+        return EXIT_FAILURE;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Hello Vulkan", 0, 0);
+
+    while(!glfwWindowShouldClose(window)) {
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return EXIT_SUCCESS;
 }
